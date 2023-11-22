f5d85838bf889baf36b001d8a1b1e87904402bfe
FREEDOM <freewhynane62@gmail.com>
2023-11-22 16:28:18 +0000

    add import and export

---
+++ src/components/index.jsx
@@ -0,0 +1,22 @@
+// Custom Button
+import CustomButton from "./CustomButton";
+
+// Auth
+import EmailVerification from "./EmailVerification";
+import LoginForm from "./LoginForm";
+import RegistrationForm from "./RegistrationForm";
+import NotFound from "./NotFound";
+
+// Invoice
+import InvoiceHeader from "./invioce/InvoiceHeader";
+import InvoiceList from "./invioce/InvoiceList";
+
+export {
+  CustomButton,
+  EmailVerification,
+  LoginForm,
+  RegistrationForm,
+  NotFound,
+  InvoiceHeader,
+  InvoiceList,
+};
---
+++ src/pages/index.jsx
@@ -0,0 +1,5 @@
+import Dashboard from "./Dashboard";
+import LandingPage from "./LandingPage";
+import Invioce from "./Invioce";
+
+export { Dashboard, LandingPage, Invioce };
