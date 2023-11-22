60bfb79b19fd84c630e289d738785715a5183f48
FREEDOM <freewhynane62@gmail.com>
2023-11-22 16:27:46 +0000

    add custom buttons

---
+++ src/components/CustomButton.jsx
@@ -0,0 +1,76 @@
+import PropTypes from "prop-types";
+import { FaPlus, FaTrash, FaDownload, FaEdit } from "react-icons/fa"; // Assuming you're using react-icons for icons
+import "../styles/CustomButton.css";
+
+const CustomButton = ({ type, onClick, children }) => {
+  let buttonContent;
+  let buttonClass = "";
+
+  switch (type) {
+    case "empty":
+      buttonContent = children;
+      buttonClass = "empty-button";
+      break;
+    case "add":
+      buttonContent = (
+        <>
+          {children}{" "}
+          <span className="icon">
+            <FaPlus />
+          </span>
+        </>
+      );
+      buttonClass = "add-button";
+      break;
+    case "delete":
+      buttonContent = (
+        <>
+          {children}{" "}
+          <span className="icon">
+            <FaTrash />
+          </span>
+        </>
+      );
+      buttonClass = "delete-button";
+      break;
+    case "download":
+      buttonContent = (
+        <>
+          {children}{" "}
+          <span className="icon">
+            <FaDownload />
+          </span>
+        </>
+      );
+      buttonClass = "download-button";
+      break;
+    case "edit":
+      buttonContent = (
+        <>
+          {children}{" "}
+          <span className="icon">
+            <FaEdit />
+          </span>
+        </>
+      );
+      buttonClass = "edit-button";
+      break;
+    default:
+      buttonContent = children;
+  }
+
+  return (
+    <button className={`custom-button ${buttonClass}`} onClick={onClick}>
+      <span className="button-content">{buttonContent}</span>
+    </button>
+  );
+};
+
+CustomButton.propTypes = {
+  type: PropTypes.oneOf(["empty", "add", "delete", "download", "edit"])
+    .isRequired,
+  onClick: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export default CustomButton;
