c74dd431a4dfc1d08f09a017cd9c9b75165f99d0
FREEDOM <freewhynane62@gmail.com>
2023-11-22 16:27:15 +0000

    modify

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,8 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Dashboard from './pages/Dashboard';
-import LandingPage from './pages/LandingPage';
-import LoginForm from './components/LoginForm';
-import RegistrationForm from './components/RegistrationForm';
-import EmailVerification from './components/EmailVerification';
-import NotFound from './components/NotFound';
+import "./App.css";
+import NavigationRoute from "./route";
 
 function App() {
-  return (
-    <Routes className="App">
-      <Route path='/' element={<LandingPage />} />
-      <Route path='/register' element={<RegistrationForm />} />
-      <Route path='/emailVerification' element={<EmailVerification />} />
-      <Route path='/logIn' element={<LoginForm />} />
-      <Route path='/dashboard' element={<Dashboard />} />
-      <Route path='*' element={<NotFound />} />
-    </Routes>
-  );
+  return <NavigationRoute />;
 }
 
 export default App;
