698f78f41a744c58c0adef197a026247b212ca35
alialaba <alialaba079@gmail.com>
2023-11-26 17:06:19 -0500

    style: style customer and feature section

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,17 +1,125 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, {useState} from "react";
 import "./LandingPage.css";
+import customa1 from "../../img/cs1.svg";
+import customa2 from "../../img/cs2.svg";
+import customa3 from "../../img/cs3.svg";
+import customa4 from "../../img/cs4.svg";
+import customa5 from "../../img/cs5.svg";
+import FeatureCard from "../../components/FeatureCard";
+import BtnElement from "../../components/BtnElement";
 
 
-const LandingPage = () => {
+const LandingPage = (props) => {
   const handleButtonClick = () => {
     // Handle button click actions here
   };
 
   return (
-    <div className="LandingPage-wrapper">
-      LandingPage
-    </div>
+    <>
+    <section className="section-hero">
+        <div className="container">
+          <div className="hero">
+            <div className="hero__content stack">
+              <h1 className="hero__title">
+                Simplify Your Small Business Payments with CrediEase!
+              </h1>
+              <p className="hero__subtitle">
+                Effortlessly manage invoices, track payments, and boost cash
+                flow.
+              </p>
+
+              <BtnElement className="hero__btn btn" text="Get Started"/>
+              <div className="customers">
+                <div className="customers__img">
+                  <img src={customa1} alt="customer-1" />
+                  <img src={customa2} alt="customer-2" />
+                  <img src={customa3} alt="customer-3" />
+                  <img src={customa4} alt="customer-4" />
+                  <img src={customa5} alt="customer-5" />
+                </div>
+                <p className="customers__text">
+                  <span>160,000+</span>  Customers across the Globe.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="container">
+        <h2 className="subheading"> CrediEase Features</h2>
+
+        <div className="features">
+          {props.ftData.map(item=>(
+            <FeatureCard key={item.id} item={item}/>
+          ))}
+        </div>
+
+        </div>
+      </section>
+
+
+      <section className="section__reviews">
+        <div className="container">
+          <h2 className="subheading">What Customers Say</h2>
+          <div className="reviews">
+          <article className="reviews__col ">
+              
+             
+              <div className="reviews__img">
+                <img src={customa1} alt="reviewer img" />
+              </div>
+              <div className="reviews__info ">
+                <p className="reviews__name">Olivia</p>
+                <span className="reviews__comment">Efficient Tracking.</span>
+              </div>
+              <p className="reviews__text">
+              “Efficient payment tracking is a must for my online store. CrediEase delivers!"
+            </p>
+           
+          </article>
+
+            <article className="reviews__col ">
+              
+             
+                <div className="reviews__img">
+                  <img src={customa3} alt="reviewer img" />
+                </div>
+                <div className="reviews__info">
+                  <p className="reviews__name">Mark</p>
+                  <span className="reviews__comment">Simplifies it all</span>
+                </div>
+                <p className="reviews__text">
+                “As a freelancer, staying on top of payments is crucial. CrediEase simplifies it all!"
+              </p>
+             
+            </article>
+
+            <article className="reviews__col ">
+              
+             
+                <div className="reviews__img">
+                  <img src={customa2} alt="reviewer img" />
+                </div>
+                <div className="reviews__info ">
+                  <p className="reviews__name">Sara</p>
+                  <span className="reviews__comment">Game-changer</span>
+                </div>
+                <p className="reviews__text">
+                "CrediEase transformed how I manage payments in my salon. It's a game-changer!"
+              </p>
+             
+            </article>
+          </div>
+        </div>
+      </section>
+
+      
+
+      
+    </>
   );
 };
 
---
+++ src/utils/ftData.js
@@ -0,0 +1,22 @@
+
+
+export const ftData = [
+    {
+        id: 1,
+        ftImg:"/public/assests/cfm.png",
+        title: "Client Profile Management",
+        text:"Effortlessly create and organize client profiles, storing essential information such as names, contact details, and payment terms. Streamline your client database for efficient transactions."
+    },
+    {
+        id: 2,
+        ftImg:"/public/assests/invoicetrack.png",
+        title: "Invoice Generation & Tracking",
+        text:"Generate unique, professional invoices tailored to each client or project. Seamlessly track payment statuses, avoid duplicates, and gain clarity on pending or paid invoices in real-time."
+    },
+    {
+        id: 3,
+        ftImg:"/public/assests/real-alert.png",
+        title: "Real-time Alerts for Overdue Payments",
+        text:"Stay on top of your finances with instant alerts for overdue payments. Receive timely notifications to ensure you never miss addressing a late payment, maintaining a healthy cash flow."
+    },
+]
