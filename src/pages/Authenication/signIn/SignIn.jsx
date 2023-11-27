1bd20c93882c700e7796cb26a11bcfb1d77adea7
julietoge <julietogechi27@gmail.com>
2023-11-27 15:33:11 +0100

    updated OGE-branch

--- src/pages/Authenication/signIn/SignIn.jsx
+++ src/pages/Authenication/signIn/SignIn.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Layout from "../AuthLayout/Layout";
 import FormField from "../../../components/formField/formField";
@@ -106,7 +106,7 @@ const SignIn = () => {
           </form>
           {/* Registration link */}
           <button className="SignIn-register-btn">
-            <Link to="/register">Create Account</Link>
+            <Link to="/createAccount">Create Account</Link>
           </button>
         </div>
       </Layout>
